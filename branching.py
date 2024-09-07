1975577f2a6e36f2a40c5297d74c9bc46416c8fc
JaviDMr10 <javidmr10@gmail.com>
2024-09-06 23:01:02 -0400

    second commit, branching.

--- branching.py
+++ branching.py
@@ -10,9 +10,9 @@ userInput = input("Enter the KW hours used: ")
 kw_hours = float(userInput)
 
 # Condition: if it is less than 1000 hours and more than that:
-if kw_hours < 1000:
+if kw_hours <= 1000:
     total = (RATE_FIRST_1000 / 100) * kw_hours
-elif kw_hours >= 1000:
+else:
     price1 = (RATE_FIRST_1000 / 100) * 1000
     price2 = (RATE_ABOVE_1000 / 100) * (kw_hours - 1000)
     total = price1 + price2
