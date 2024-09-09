343aa0bac8ffd797490a65aa976adefe57f33141
JaviDMr10 <javidmr10@gmail.com>
2024-09-09 14:33:29 -0400

    third commit, branching.

--- branching.py
+++ branching.py
@@ -4,12 +4,12 @@
 RATE_FIRST_1000 = 7.633
 RATE_ABOVE_1000 = 9.259
 
-#Enter the user inputs: 1500, 764, 1215, and 812.
+#enter user inputs: 1500, 764, 1215, and 812.
 userInput = input("Enter the KW hours used: ")
 
 kw_hours = float(userInput)
 
-# Condition: if it is less than 1000 hours and more than that:
+# Condition: if the time is less than 1000 hours and more than 1000 hours:
 if kw_hours <= 1000:
     total = (RATE_FIRST_1000 / 100) * kw_hours
 else:
